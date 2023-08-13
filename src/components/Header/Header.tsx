d7b2c5a063fcaffbc4df2ccdc9577af6f95775f1
JeffOrdze <jeff.ordze@live.ca>
2023-08-13 10:43:34 -0600

    add header responsive

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,14 +4,14 @@ import MobileMenu from "../MobileMenu/MobileMenu";
 import ThemeToggle from "../ThemeToggle/ThemeToggle";
 
 const Header = () => {
-    return (
-        <header className="relative flex h-16 gap-4 pt-6 px-4 sm:px-8 lg:px-12">
-            <ProfileIcon/>
-            <Navbar/>
-            <MobileMenu/>
-            <ThemeToggle/>
-        </header>
-    );
+  return (
+    <header className="relative flex h-16 gap-4 pt-6 px-4 sm:px-8 lg:px-12">
+      <ProfileIcon />
+      <Navbar />
+      <MobileMenu />
+      <ThemeToggle />
+    </header>
+  );
 };
 
 export default Header;
