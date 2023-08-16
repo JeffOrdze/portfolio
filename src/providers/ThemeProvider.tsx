0f0e1a4823ed290676f14a3a0bc72ae70339ff42
JeffOrdze <jeff.ordze@live.ca>
2023-08-16 15:22:03 -0600

    fix height issue with main

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -9,7 +9,7 @@ export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
     };
 
     const currentTheme = localStorage.getItem("theme");
-
+    
     useEffect(() => {
         if (!currentTheme) {
             if (
@@ -25,7 +25,7 @@ export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
 
     useEffect(() => {
         localStorage.setItem("theme", darkTheme ? "dark" : "light");
-        document.documentElement.className = darkTheme ? "dark" : "light";
+        document.documentElement.className = darkTheme ? "dark h-full" : "light h-full";
     }, [darkTheme]);
 
     return (
